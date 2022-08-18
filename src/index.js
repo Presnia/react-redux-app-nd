b2289a80f07a1a648cefaba83f246a7f55b7e7fd
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 09:58:03 +0300

    A simple example of a higher-order function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
+
     function someFn() {
-        return 'app';
+        return function() {
+            return 'app';
+        }
     }
-    const fn = someFn();
-    return <h1> {fn}</h1>
+    function fn(func) {
+        return func();
+    }
+
+    return <h1> {fn(someFn())} </h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
