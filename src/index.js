eed251c66adf245a78656affdec98fcd0ea7f0e1
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 13:07:08 +0300

    The dispatcher function is created

--- src/index.js
+++ src/index.js
@@ -5,16 +5,30 @@ function createStore(initialState) {
     let state = initialState;
 
     const getState = () => state;
-    return { getState };
+
+    const dispatch = action => {
+        if (action.type === 'task/completed') {
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex(el => el.id === action.payload.id);
+            newArray[elementIndex].completed = true;
+            state = newArray;
+            console.log(state);
+        }
+    }
+
+    return { getState, dispatch };
 }
 const store = createStore([{id: 1, description:'Task 1', completed: false}])
 
 const App = () => {
-    store.getState();
-    console.log(store.getState())
+    console.log(store.getState());
+
     return (
         <>
             <h1>App</h1>
+            <button
+                onClick={() => store.dispatch({type: 'task/completed', payload: { id: 1 }})}
+            >Complete</button>
             <h2>Something about app</h2>
         </>
     );
