a9c367929e1bd0b018aeae8b20dd2e9a8f2e5fc6
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 16:08:37 +0300

    Refactored with logical blocks

---
+++ src/store/createStore.js
@@ -0,0 +1,20 @@
+export default function createStore(reducer, initialState) {
+    let state = initialState;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = action => {
+        state = reducer(state, action);
+        for (let i = 0; i < listeners.length; i++) {
+            const listener = listeners[i];
+            listener();
+        }
+    }
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+    }
+
+    return { getState, dispatch, subscribe };
+}
