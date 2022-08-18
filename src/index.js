016a653e6bccd79c4f3dc83a0e6d2cdc5442c80a
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 12:10:52 +0300

    Starting to create the Redux prototype

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-    const [state, setState] = useState({});
-    //setState(prevState => ({...prevState, id:'123'})); creating a new object with some new property
-    const obj1 = {id: 2};
-    //const obj2 = {obj1}; creating a copy of obj1
-    const obj2 = {...obj1}; //creating a new object with all the properties of obj1
 
     return (
         <>
