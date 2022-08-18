33c259b8eac0c4eeffc7610f8b124920d238b1df
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 09:44:43 +0300

    Remembering the functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const App = () => {
+    function fn() {
+        return 'app';
+    }
+    function someFn(func) {
+        return func();
+    }
+    return <h1> {someFn(fn)}</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-        Hi Bros!!!
-    </div>
+    <App />
   </React.StrictMode>
 );
