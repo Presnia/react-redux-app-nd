e69261b0affa2adef123e0876aeb5a1500b1b8c0
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 10:59:23 +0300

    Refactored with fp from lodash

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { compose, pipe } from 'lodash/fp';
 
 const App = () => {
     const x = 2;
     const double = num => num * 2;
     const square = num => num * num;
     const half = num => num / 2;
+    const MathCalculate =  compose(square, double);
 
     return (
         <>
-            <h1>{square(double(x))}</h1>
-            <h2>{half(square(double(x)))}</h2>
+            <h1>{MathCalculate(x)}</h1>
+            <h2>{half(MathCalculate(x))}</h2>
         </>
     );
 }
