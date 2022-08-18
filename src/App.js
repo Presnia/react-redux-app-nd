535cb090313678ecd2e2ec4716372b74be2734bd
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 17:28:23 +0300

    Added the styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import createStore from "./store/createStore";
 import taskReducer from "./store/taskReducer";
 import * as actions from "./store/actionTypes";
 
+import s from './App.module.css'
+
 const initialState = [
     {id: 1, title:'Task 1', completed: false},
     {id: 2, title:'Task 2', completed: false}
@@ -34,13 +36,22 @@ const App = () => {
     }
 
     return (
-        <>
+        <div className={s.container}>
             <h1>App</h1>
             <ul>
                 {state.map(el => (
                     <li key={el.id}>
-                        <p>{el.title}</p>
-                        <p>{`Completed: ${el.completed}`}</p>
+                        <p className={s.title}>{el.title}</p>
+                        <p>
+                            Completed:
+                            <span
+                                style={
+                                    el.completed === false ? {color: 'red'} : {color: 'lightgreen'}
+                                }
+                            >
+                             {` ${el.completed}`}
+                            </span>
+                        </p>
                         <button
                             onClick={() => handleTask(el.id)}
                         >
@@ -51,11 +62,11 @@ const App = () => {
                         >
                             Change title
                         </button>
-                        <hr/>
+                        <hr />
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     );
 }
 
