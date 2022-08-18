daacd6e36ff86cef34f50381a314065595a280ef
Andrei Dzebysh <presnia@yahoo.com>
2022-08-18 10:42:13 +0300

    Added the half function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const App = () => {
     const x = 2;
     const double = num => num * 2;
     const square = num => num * num;
+    const half = num => num / 2;
 
-    return <h1>{square(double(x))}</h1>;
+    return (
+        <>
+            <h1>{square(double(x))}</h1>
+            <h2>{half(square(double(x)))}</h2>
+        </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
