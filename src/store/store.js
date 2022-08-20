24301d2be300305c30fcf9580b361ca3e7eb11d3
Andrei Dzebysh <presnia@yahoo.com>
2022-08-20 11:32:16 +0300

    The reducer is works now with the createRuducer func

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,6 @@
 import {createStore} from "redux";
 import reducer from "./tasks";
 
-const initialState = [
-    {id: 1, title:'Task 1', completed: false},
-    {id: 2, title:'Task 2', completed: false},
-    {id: 3, title:'Task 3', completed: false},
-];
-
 export default function configureStore() {
-    return createStore(reducer, initialState);
+    return createStore(reducer);
 };
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,4 +1,10 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const initialState = [
+    {id: 1, title:'Task 1', completed: false},
+    {id: 2, title:'Task 2', completed: false},
+    {id: 3, title:'Task 3', completed: false},
+];
 
 const toUpdate = createAction('task/updated');
 const toDelete = createAction('task/deleted');
@@ -15,21 +21,15 @@ export function taskDeleted(id) {
     return toDelete({id});
 }
 
-function reducer(state, action) {
-    switch (action.type) {
-        case toUpdate.type: {
-            const newArray = [...state];
-            const elementIndex = newArray.findIndex(el => el.id === action.payload.id);
-            newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
-            return newArray;
-        }
-        case toDelete.type: {
+const reducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase(toUpdate, (state, action) => {
+            const elementIndex = state.findIndex(el => el.id === action.payload.id);
+            state[elementIndex] = {...state[elementIndex], ...action.payload}
+        })
+        .addCase(toDelete, (state, action) => {
             return state.filter(el => el.id !== action.payload.id);
-        }
-
-        default:
-            return state;
-    }
-}
+        })
+});
 
 export default reducer;
