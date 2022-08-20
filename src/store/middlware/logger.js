0ff52e4c0a8610a9e11b52e966fc005f42d3cc36
Andrei Dzebysh <presnia@yahoo.com>
2022-08-20 14:28:46 +0300

    The store has been refactored with logger as middlware

--- src/store/middlware/logger.js
+++ src/store/middlware/logger.js
@@ -1,11 +1,7 @@
 export function logger(store) {
     return function wrapDispatch(next) {
         return function handleAction(action) {
-            console.log(store)
-            console.log(next)
-            console.log(action)
-
-            return next(action)
-        }
-    }
+            return next(action);
+        };
+    };
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import reducer from "./tasks";
+import { logger } from "./middlware/logger";
+
+const middlewareEnhancer = applyMiddleware(logger);
 
 export default function configureStore() {
     return createStore(
         reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        compose(
+            middlewareEnhancer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )
     );
 };
