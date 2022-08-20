87362377635d8eab9e57114108aa2c069313d0e2
Andrei Dzebysh <presnia@yahoo.com>
2022-08-20 11:53:37 +0300

    The actions and the reducer are implemented using the createSlice method

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,13 +1,10 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = [
     {id: 1, title:'Task 1', completed: false},
     {id: 2, title:'Task 2', completed: false},
     {id: 3, title:'Task 3', completed: false},
 ];
-
-const toUpdate = createAction('task/updated');
-const toDelete = createAction('task/deleted');
 
 export function taskCompleted(id) {
     return toUpdate({id, completed: true});
@@ -21,15 +18,19 @@ export function taskDeleted(id) {
     return toDelete({id});
 }
 
-const reducer = createReducer(initialState, (builder) => {
-    builder
-        .addCase(toUpdate, (state, action) => {
+const taskSlice = createSlice({
+    name: 'task',
+    initialState,
+    reducers: {
+        toUpdate(state, action) {
             const elementIndex = state.findIndex(el => el.id === action.payload.id);
-            state[elementIndex] = {...state[elementIndex], ...action.payload}
-        })
-        .addCase(toDelete, (state, action) => {
+            state[elementIndex] = {...state[elementIndex], ...action.payload};
+        },
+        toDelete(state, action) {
             return state.filter(el => el.id !== action.payload.id);
-        })
+        }
+    }
 });
 
-export default reducer;
+export const { toUpdate, toDelete } = taskSlice.actions;
+export default taskSlice.reducer;
